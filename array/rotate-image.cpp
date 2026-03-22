9188cfbbed67a471c3e55e2156765cd991a4ebe9
Lyx-maker6 <lyx_2006@foxmail.com>
2026-03-22 09:21:05 +0800

    LC-rotate-image [Runtime: 3 ms; Memory: 10.2 MB]

---
+++ array/rotate-image.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = i+1; j < row; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < row / 2; j++)
+            {
+                swap(matrix[i][j], matrix[i][row - 1 - j]);
+            }
+        }
+
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < row; j++)
+            {
+                cout<<matrix[i][j];
+            }
+        }
+    }
+};
