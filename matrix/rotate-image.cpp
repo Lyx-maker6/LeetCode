fab03301b054ac45839908548530779161293010
Lyx-maker6 <lyx_2006@foxmail.com>
2026-03-22 13:12:42 +0800

    LC-rotate-image [Runtime: 0 ms; Memory: 10.2 MB]

--- matrix/rotate-image.cpp
+++ matrix/rotate-image.cpp
@@ -10,16 +10,16 @@ public:
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-        // for(int i = 0; i < row; i++)
-        // {
-        //     for(int j = 0; j < row / 2; j++)
-        //     {
-        //         swap(matrix[i][j], matrix[i][row - 1 - j]);
-        //     }
-        // }
-        for (int k = 0 ; k < row ; k++){
-            reverse(matrix[k].begin(), matrix[k].end());
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < row / 2; j++)
+            {
+                swap(matrix[i][j], matrix[i][row - 1 - j]);
+            }
         }
+        // for (int k = 0 ; k < row ; k++){
+        //     reverse(matrix[k].begin(), matrix[k].end());
+        // }
 
         
     }
