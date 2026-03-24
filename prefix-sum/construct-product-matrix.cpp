abdd36e07d60a7f5fdd3a44c30633beb2679659c
Lyx-maker6 <lyx_2006@foxmail.com>
2026-03-24 20:00:15 +0800

    LC-construct-product-matrix [Runtime: 43 ms; Memory: 132 MB]

--- prefix-sum/construct-product-matrix.cpp
+++ prefix-sum/construct-product-matrix.cpp
@@ -5,21 +5,28 @@ public:
         int c = grid[0].size();
 
         vector<vector<int>> ans(r, vector<int>(c));
+        long long pre = 1;//前缀积
+        long long last = 1;//后缀积
 
-        long long res = 1;
-
-        for(int i = 0; i < r; i++)
-            for(int j = 0; j < c; j++)
-                res *= grid[i][j];
-        
+        //从前往后算
         for(int i = 0; i < r; i++)
         {
             for(int j = 0; j < c; j++)
             {
-                int tp = res / grid[i][j];
-                ans[i][j] = tp % 12345;
+                ans[i][j] = pre;//pre相当于接力棒，先把接力棒传给当前这个数
+                pre = (pre * grid[i][j]) % 12345;//更新值
             }
         }
+        //从后往前算
+        for(int i = r-1; i >= 0; i--)
+        {
+            for(int j = c-1; j >= 0; j--)
+            {
+                ans[i][j] = (ans[i][j] * last) % 12345;
+                last = (last * grid[i][j]) % 12345;
+            }
+        }
+
         return ans;
             
     }
