4d0d48b3d5ad9c12c87b0ab75cf24edd0b6e0ae7
Lyx-maker6 <lyx_2006@foxmail.com>
2026-03-30 09:47:27 +0800

    LC-check-if-strings-can-be-made-equal-with-operations-ii [Runtime: 2307 ms; Memory: 19.4 MB]

--- hash-table/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ hash-table/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,25 +2,19 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
         int n = s1.length();
-        string saa, sab, sba, sbb;
-        for(int i = 0; i < n; ++i){
-            if(i % 2 ==0){
-                saa += s1[i];
-                sba += s2[i];
+        for(int i = 0;i < n;i++)
+        {
+            if (s1[i] == s2[i]) continue;
+            for(int j = i + 2;j < n;j +=2)
+            {
+                if(s1[j] == s2[i]){
+                    swap(s1[i],s1[j]);
+                    break;
+                }
+
             }
-            else{
-                sab += s1[i];
-                sbb += s2[i];
-            }
+            if(s1[i] != s2[i]) return false;
         }
-        sort(saa.begin(), saa.end());
-        sort(sba.begin(), sba.end());
-        if(saa != sba)
-            return false;
-        sort(sab.begin(), sab.end());
-        sort(sbb.begin(), sbb.end());
-        if(sab != sbb)
-            return false;
-        return true;
+        return s1==s2;
     }
 };
