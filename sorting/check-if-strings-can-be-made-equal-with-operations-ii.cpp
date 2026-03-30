d739134daeed5a0ae039ab4c213b11e7cc5a14a0
Lyx-maker6 <lyx_2006@foxmail.com>
2026-03-30 09:17:27 +0800

    LC-check-if-strings-can-be-made-equal-with-operations-ii [Runtime: N/A; Memory: N/A]

---
+++ sorting/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        int n = s1.length();
+        string res = s1;
+        for(int i = 0;i < n;i++)
+        {
+            for(int j = 1;j < n;j++,(j-i)%2==0)
+            {
+                swap(res[i],res[j]);
+                if(res==s2)
+                return true;
+            }
+            
+        }
+        return false;
+    }
+};
