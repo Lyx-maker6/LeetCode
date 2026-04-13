c511050bfa06839b71808fa48895b7c6bade9a75
Lyx-maker6 <lyx_2006@foxmail.com>
2026-04-13 14:43:31 +0800

    LC-minimum-distance-to-the-target-element [Runtime: 4 ms; Memory: 13.3 MB]

--- array/minimum-distance-to-the-target-element.cpp
+++ array/minimum-distance-to-the-target-element.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int getMinDistance(vector<int>& nums, int target, int start) {
         int n = nums.size();
-        int ans = INT_MAX;
+        int ans = 1e5;
 
         for(int i = 0;i < n;i++)
         {
