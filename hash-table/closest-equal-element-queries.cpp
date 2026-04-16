099b0e99163e70e9c80ed8ed8ef56282add8d572
Lyx-maker6 <lyx_2006@foxmail.com>
2026-04-16 12:03:23 +0800

    LC-closest-equal-element-queries [Runtime: N/A; Memory: N/A]

---
+++ hash-table/closest-equal-element-queries.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        int n = queries.size();
+        vector<int> ans;
+
+        int m = nums.size();
+       
+        for(auto q : queries)
+        {
+            int min_v = INT_MAX;
+            int d = 0;
+
+            for(int i = 0;i < nums.size();i++)
+            {
+                // 必须满足 nums[i] == nums[q] 且 i != q
+                if(i != q && nums[i] == nums[q])
+                {
+                    d = abs(i - q);
+                    int re_d = m - d;
+                    min_v = min(min_v,min(d,re_d));
+                }
+            }
+            if(min_v == INT_MAX) ans.push_back(-1);
+            else ans.push_back(min_v);
+        }
+        return ans;
+    }
+};
