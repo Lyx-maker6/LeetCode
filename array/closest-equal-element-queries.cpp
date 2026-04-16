90d261f7bf5e98c5c812c248cec10592ea95e86f
Lyx-maker6 <lyx_2006@foxmail.com>
2026-04-16 12:10:18 +0800

    LC-closest-equal-element-queries [Runtime: 209 ms; Memory: 216.7 MB]

--- array/closest-equal-element-queries.cpp
+++ array/closest-equal-element-queries.cpp
@@ -28,11 +28,11 @@ public:
 
 
         int m = nums.size();
-        unordered_map<int, vector<int>> pos_map;
-        for (int i = 0; i < m; i++) pos_map[nums[i]].push_back(i);
+    unordered_map<int, vector<int>> pos_map;
+    for (int i = 0; i < m; i++) pos_map[nums[i]].push_back(i);
 
-        vector<int> ans;
-        for (int q : queries) {
+    vector<int> ans;
+    for (int q : queries) {
         int targetVal = nums[q];
         if (pos_map[targetVal].size() == 1) { // 如果该数值只有一个索引，则必无解
             ans.push_back(-1); continue;
@@ -42,6 +42,37 @@ public:
         auto it = lower_bound(indices.begin(), indices.end(), q);
         int min_v = INT_MAX;
 
-        // 尝试检查右侧
+        // 尝试检查右侧元素
+        if (it != indices.end()) {
+            // 如果 lower_bound 正好找到 q 自己，那我们就尝试右边那个元素，或者如果右边没有，尝试左边那个元素
+            int next_idx = (*it == q) ? (next(it) == indices.end() ? -1 : *next(it)) : *it;
+            if (next_idx != -1) {
+                int d = abs(next_idx - q);
+                min_v = min(min_v, min(d, m - d));
+            }
+        }
+        
+        // 尝试检查左侧元素
+        if (it != indices.begin()) {
+            int left_idx = (prev(it) == indices.end()) ? -1 : *prev(it); // 在 lower_bound it 上做 prev 是安全的
+             if (left_idx != -1 && left_idx != q) { // 这里多加一个逻辑防御
+                int d = abs(left_idx - q);
+                min_v = min(min_v, min(d, m - d));
+            }
+        }
+        
+        // 关键补充：检查有序列表的首尾连接 (针对 q=0 或 q=m-1 这样的情况)
+        if (indices.front() != q) {
+             int d = abs(indices.front() - q);
+             min_v = min(min_v, min(d, m - d));
+        }
+        if (indices.back() != q) {
+             int d = abs(indices.back() - q);
+             min_v = min(min_v, min(d, m - d));
+        }
+
+        ans.push_back(min_v == INT_MAX ? -1 : min_v);
+    }
+    return ans;
     }
 };
