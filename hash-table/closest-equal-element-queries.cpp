42ac3423cdb4c9658ba0c11eb825222c47958fb6
Lyx-maker6 <lyx_2006@foxmail.com>
2026-04-16 12:08:47 +0800

    LC-closest-equal-element-queries [Runtime: N/A; Memory: N/A]

--- hash-table/closest-equal-element-queries.cpp
+++ hash-table/closest-equal-element-queries.cpp
@@ -28,36 +28,20 @@ public:
 
 
         int m = nums.size();
-        unordered_map<int, vector<int>> pos_map;
-        for (int i = 0; i < m; i++) {
-            pos_map[nums[i]].push_back(i);
+    unordered_map<int, vector<int>> pos_map;
+    for (int i = 0; i < m; i++) pos_map[nums[i]].push_back(i);
+
+    vector<int> ans;
+    for (int q : queries) {
+        int targetVal = nums[q];
+        if (pos_map[targetVal].size() == 1) { // 如果该数值只有一个索引，则必无解
+            ans.push_back(-1); continue;
         }
 
-        vector<int> ans;
-        for (int q : queries) {
-            const vector<int>& indices = pos_map[nums[q]];
-            
-            // 使用二分查找找到第一个大于或等于 q 的位置
-            auto it = lower_bound(indices.begin(), indices.end(), q);
-            
-            int min_v = INT_MAX;
-            
-            // 检查 it 指向的元素 (右侧最近)
-            if (it != indices.end() && *it != q) {
-                int d = abs(*it - q);
-                min_v = min(min_v, min(d, m - d));
-            }
-            // 检查 it 的前一个元素 (左侧最近)
-            if (it != indices.begin()) {
-                int left_val = *prev(it);
-                if (left_val != q) {
-                    int d = abs(left_val - q);
-                    min_v = min(min_v, min(d, m - d));
-                }
-            }
-            
-            ans.push_back(min_v == INT_MAX ? -1 : min_v);
-        }
-        return ans;
+        const vector<int>& indices = pos_map[targetVal];
+        auto it = lower_bound(indices.begin(), indices.end(), q);
+        int min_v = INT_MAX;
+
+        // 尝试检查右侧
     }
 };
