2b393ff40e803f985e59bb1187a7a43032817343
Lyx-maker6 <lyx_2006@foxmail.com>
2026-04-17 22:22:00 +0800

    LC-minimum-absolute-distance-between-mirror-pairs [Runtime: 104 ms; Memory: 132 MB]

--- hash-table/minimum-absolute-distance-between-mirror-pairs.cpp
+++ hash-table/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -30,25 +30,19 @@ public:
         // }
         // return (min_v == INT_MAX)? -1 : min_v;
 
-        
-        // 只需要一个备忘录：记录数值 -> 最后出现的下标
-        unordered_map<int, int> last_seen;
 
-        for (int i = 0; i < n; i++) {
-            int val = nums[i];
-            int rev = reverse(val);
+        // key: 前面某个数反转后的结果, value: 该数原本的下标
+        unordered_map<int, int> rev_map;
 
-            // 核心修正：对称性检查
-            // 只有当“反转再反转”能回到原值时，这个数才可能拥有镜像
-            if (reverse(rev) == val) {
-                // 检查它的镜像（rev）之前是否出现过
-                if (last_seen.count(rev)) {
-                    min_v = min(min_v, i - last_seen[rev]);
-                }
+        for (int j = 0; j < n; j++) {
+            // 1. 检查当前的数，是否是前面某个数反转后的结果
+            if (rev_map.count(nums[j])) {
+                min_v = min(min_v, j - rev_map[nums[j]]);
             }
 
-            // 无论是否匹配，都记录当前数值出现的位置
-            last_seen[val] = i;
+            // 2. 将当前数反转后的结果存入 map，供后面的数匹配
+            // 注意：我们要找的是最小距离，所以如果反转值相同，更新为靠后的下标
+            rev_map[reverse(nums[j])] = j;
         }
 
         return (min_v == INT_MAX) ? -1 : min_v;
