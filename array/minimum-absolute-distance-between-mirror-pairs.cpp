175f0ed397a327180ee3fe8cfe62a7d06cc5c243
Lyx-maker6 <lyx_2006@foxmail.com>
2026-04-17 22:14:21 +0800

    LC-minimum-absolute-distance-between-mirror-pairs [Runtime: N/A; Memory: N/A]

--- array/minimum-absolute-distance-between-mirror-pairs.cpp
+++ array/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -17,19 +17,46 @@ public:
         int n = nums.size();
         int cnt = 0;
         int min_v = INT_MAX;
-        for(int i = 0;i < n;i++)
-        {
-            int tp = reverse(nums[i]);
+        // for(int i = 0;i < n;i++)
+        // {
+        //     int tp = reverse(nums[i]);
 
-            for(int j = i + 1;j < n; j++)
-            {
-                if(tp == nums[j])
-                {
-                    min_v = min(min_v,abs(i - j));            
-                }
-            }
-        }
-        return (min_v == INT_MAX)? -1 : min_v;
+        //     for(int j = i + 1;j < n; j++)
+        //     {
+        //         if(tp == nums[j])
+        //         {
+        //             min_v = min(min_v,abs(i - j));            
+        //         }
+        //     }
+        // }
+        // return (min_v == INT_MAX)? -1 : min_v;
         
+        // key: 数值, value: 该数值最后出现的下标
+        unordered_map<int, int> last_seen_val;
+        // key: 反转后的值, value: 产生该反转值的原数下标
+        unordered_map<int, int> last_seen_rev;
+
+        for (int i = 0; i < n; i++) {
+            int val = nums[i];
+            int rev = reverse(val);
+
+            // 情况 A：当前的 nums[i] 刚好是之前某个 nums[p] 的反转
+            // 即：reverse(nums[p]) == nums[i]
+            if (last_seen_rev.count(val)) {
+                min_v = min(min_v, i - last_seen_rev[val]);
+            }
+            
+            // 情况 B：之前的某个 nums[p] 刚好是当前 nums[i] 的反转
+            // 即：nums[p] == reverse(nums[i])
+            if (last_seen_val.count(rev)) {
+                min_v = min(min_v, i - last_seen_val[rev]);
+            }
+
+            // 更新当前数值及其反转值的最新位置
+            last_seen_val[val] = i;
+            last_seen_rev[rev] = i;
+        }
+
+        return (min_v == INT_MAX) ? -1 : min_v;
     }
 };
