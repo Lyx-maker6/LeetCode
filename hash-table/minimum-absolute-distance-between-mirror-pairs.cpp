9acfa307d9481768ccdfc3651f40d110c5408b6e
Lyx-maker6 <lyx_2006@foxmail.com>
2026-04-17 22:04:25 +0800

    LC-minimum-absolute-distance-between-mirror-pairs [Runtime: N/A; Memory: N/A]

--- hash-table/minimum-absolute-distance-between-mirror-pairs.cpp
+++ hash-table/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -7,8 +7,6 @@ public:
         {
             int tp = x % 10;
             x /= 10;
-            if(tp == 0)
-                continue;
             ans = ans * 10 + tp;
         }
         return ans;
@@ -18,7 +16,7 @@ public:
     int minMirrorPairDistance(vector<int>& nums) {
         int n = nums.size();
         int cnt = 0;
-        int flag = 0;
+        int min_v = INT_MAX;
         for(int i = 0;i < n;i++)
         {
             for(int j = i + 1;j < n; j++)
@@ -27,16 +25,11 @@ public:
 
                 if(tp == nums[j])
                 {
-                    flag = 1;
-                    if(abs(i - j) ==1)
-                    {
-                        cnt++;
-                    }
+                    min_v = min(min_v,abs(i - j));            
                 }
             }
         }
-        if(flag) return cnt;
-        else return -1;
+        return min_v;
         
     }
 };
