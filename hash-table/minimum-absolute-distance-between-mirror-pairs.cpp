be12e62eca77d38e5f66d567ec8a2629d72d3df0
Lyx-maker6 <lyx_2006@foxmail.com>
2026-04-17 22:19:03 +0800

    LC-minimum-absolute-distance-between-mirror-pairs [Runtime: N/A; Memory: N/A]

--- hash-table/minimum-absolute-distance-between-mirror-pairs.cpp
+++ hash-table/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -15,7 +15,6 @@ public:
     
     int minMirrorPairDistance(vector<int>& nums) {
         int n = nums.size();
-        int cnt = 0;
         int min_v = INT_MAX;
         // for(int i = 0;i < n;i++)
         // {
@@ -30,31 +29,26 @@ public:
         //     }
         // }
         // return (min_v == INT_MAX)? -1 : min_v;
+
         
-        // key: 数值, value: 该数值最后出现的下标
-        unordered_map<int, int> last_seen_val;
-        // key: 反转后的值, value: 产生该反转值的原数下标
-        unordered_map<int, int> last_seen_rev;
+        // 只需要一个备忘录：记录数值 -> 最后出现的下标
+        unordered_map<int, int> last_seen;
 
         for (int i = 0; i < n; i++) {
             int val = nums[i];
             int rev = reverse(val);
 
-            // 情况 A：当前的 nums[i] 刚好是之前某个 nums[p] 的反转
-            // 即：reverse(nums[p]) == nums[i]
-            if (last_seen_rev.count(val)) {
-                min_v = min(min_v, i - last_seen_rev[val]);
-            }
-            
-            // 情况 B：之前的某个 nums[p] 刚好是当前 nums[i] 的反转
-            // 即：nums[p] == reverse(nums[i])
-            if (last_seen_val.count(rev)) {
-                min_v = min(min_v, i - last_seen_val[rev]);
+            // 核心修正：对称性检查
+            // 只有当“反转再反转”能回到原值时，这个数才可能拥有镜像
+            if (reverse(rev) == val) {
+                // 检查它的镜像（rev）之前是否出现过
+                if (last_seen.count(rev)) {
+                    min_v = min(min_v, i - last_seen[rev]);
+                }
             }
 
-            // 更新当前数值及其反转值的最新位置
-            last_seen_val[val] = i;
-            last_seen_rev[rev] = i;
+            // 无论是否匹配，都记录当前数值出现的位置
+            last_seen[val] = i;
         }
 
         return (min_v == INT_MAX) ? -1 : min_v;
