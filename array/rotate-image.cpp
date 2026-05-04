b1b120a77b8175db2419665c6a64b9e66a63ca36
Lyx-maker6 <lyx_2006@foxmail.com>
2026-05-04 21:55:30 +0800

    LC-rotate-image [Runtime: 0 ms; Memory: 10.2 MB]

--- array/rotate-image.cpp
+++ array/rotate-image.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int row = matrix.size();
+        int n = matrix.size();
 
-        for(int i = 0; i < row; i++)
-        {
-            for(int j = i+1; j < row; j++)
-            {
+        // 1. 转置矩阵：沿着主对角线翻转
+        for (int i = 0; i < n; i++) {
+            // 注意 j 从 i 开始，只处理对角线的一侧，否则换过去又换回来了
+            for (int j = i + 1; j < n; j++) {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-        for(int i = 0; i < row; i++)
-        {
-            for(int j = 0; j < row / 2; j++)
-            {
-                swap(matrix[i][j], matrix[i][row - 1 - j]);
-            }
-        }
-        // for (int k = 0 ; k < row ; k++){
-        //     reverse(matrix[k].begin(), matrix[k].end());
-        // }
 
-        
+        // 2. 左右镜像：反转每一行
+        for (int i = 0; i < n; i++) {
+            // 使用 reverse 直接反转整行
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
